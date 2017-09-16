9d0e8361298cca1c6653b18352a4ae4c7f1a86ed
ytakaya <ytakaya@mikilab.doshisha.ac.jp>
2017-09-16 17:38:20 +0900

    add HttpAsync

--- android/KonicaMinolta_Hackathon/app/src/main/java/com/isdl/konicaminolta_hackathon/MainActivity.java
+++ android/KonicaMinolta_Hackathon/app/src/main/java/com/isdl/konicaminolta_hackathon/MainActivity.java
@@ -2,6 +2,12 @@ package com.isdl.konicaminolta_hackathon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static com.isdl.konicaminolta_hackathon.R.id.ToRecognizer;
+import static com.isdl.konicaminolta_hackathon.R.id.ToSleeper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final HttpAsync getAsync= new HttpAsync();
+
+        Button toSleeper = (Button)findViewById(ToSleeper);
+        Button toRecognizer = (Button)findViewById(ToRecognizer);
+
+        toSleeper.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                System.out.println("あああああああああああああああああ");
+
+                //params[0]：endpoint , params[1]：GET or POST
+                String[] params = { "/", "GET" };
+                getAsync.execute(params);
+
+//                ここに寝たい人へのintent遷移or寝たい情報の送信処理
+            }
+        });
+
+
+
+        toRecognizer.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaa");
+//                ここに承認する人へのintent遷移or承認の送信処理
+            }
+        });
     }
 }
